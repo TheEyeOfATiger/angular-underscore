7367ea78c9cb556a380fc79b648d67f23734a8f3
Lazar Samuel <lazar.samuel@gmail.com>
2018-08-22 13:50:50 +0300

    First
    added required files to the package.

---
+++ angular-underscore-module.js
@@ -0,0 +1,10 @@
+/**
+ * Let's you use underscore as a service from a controller.
+ * Got the idea from: http://stackoverflow.com/questions/14968297/use-underscore-inside-controllers
+ * @author: Andres Aguilar https://github.com/andresesfm
+ */
+
+angular.module('underscore', []).factory('_', ['$window', function($window)
+{
+    return $window._2; // assumes underscore has already been loaded on the page
+}]);
