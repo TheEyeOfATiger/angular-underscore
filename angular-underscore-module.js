8f6e19dfc52ee427142e8718aa2094862c32f8b0
Lazar Samuel <lazar.samuel@gmail.com>
2018-08-24 11:16:17 +0300

    _.get
    Added _.get function to stop making longs if, when trying to assign a deep path value from an object.

--- angular-underscore-module.js
+++ angular-underscore-module.js
@@ -6,5 +6,41 @@
 
 angular.module('underscore', []).factory('_', ['$window', function($window)
 {
+    $window._2.mixin({
+        get: function(obj, path) {
+            if (!obj && !path) {
+                return undefined;
+            } else {
+                var paths;
+
+                if (!_.isEmpty(path.match(/^\[\d\]/))) {
+                    paths = path.replace(/^[\[\]]/g, '').split(/\./);
+                    nPath = _.first(paths[0].replace(/\]/, ''));
+                } else {
+                    paths = path.split(/[\.\[]/);
+                    nPath = _.first(paths);
+                }
+
+                remainingPath = _.reduce(_.rest(paths), function(result, item) {
+                    if (!_.isEmpty(item)) {
+                        if (item.match(/^\d\]/)) {
+                            item = "[" + item;
+                        }
+                        result.push(item);
+                    }
+
+                    return result;
+                }, []).join('.');
+
+                if (_.isEmpty(remainingPath)) {
+                    return obj[nPath];
+                } else {
+                    return _.has(obj, nPath) && _.get(obj[nPath], remainingPath);
+                }
+            }
+        }
+    });
+
+    $window._ = $window._2;
     return $window._2; // assumes underscore has already been loaded on the page
 }]);
